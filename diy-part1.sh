9e3c39661fe18dec3056055937272c7e9b569dd1
wang950 <74122481+wang950@users.noreply.github.com>
2020-11-29 08:45:08 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -5,12 +5,18 @@
 # This is free software, licensed under the MIT License.
 # See /LICENSE for more information.
 #
+#=============================================================
 # https://github.com/P3TERX/Actions-OpenWrt
 # File name: diy-part1.sh
 # Description: OpenWrt DIY script part 1 (Before Update feeds)
 #
+# Lisence: MIT
+# Author: P3TERX
+# Blog: https://p3terx.com
+#=============================================================
 
 # Uncomment a feed source
+# fw876/helloworld
 sed -i 's/^#\(.*helloworld\)/\1/' feeds.conf.default
 
 # Add a feed source
